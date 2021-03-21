ed6556e8034a7a5452cae79c8a2176b1327ccfe1
Niek <n.veenstra02@gmail.com>
2021-03-21 20:32:00 +0100

    finished debug UI lesson

--- 08-debug-ui/src/script.js
+++ 08-debug-ui/src/script.js
@@ -3,6 +3,20 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import gsap from "gsap";
 
+import * as dat from "dat.gui";
+
+// Debug
+const gui = new dat.GUI();
+
+gui.hide();
+
+const parameters = {
+  color: 0xff00ff,
+  spin: () => {
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+  },
+};
+
 /**
  * Base
  */
@@ -16,10 +30,25 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+const material = new THREE.MeshBasicMaterial({ color: parameters.color, wireframe: true });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// debug
+// gui.add(mesh.position, "x", -3, 3, 0.01);
+gui.add(mesh.position, "x").min(-3).max(3).step(0.01).name("elevation");
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01);
+gui.add(mesh.position, "z").min(-3).max(3).step(0.01);
+
+gui.add(mesh, "visible");
+
+gui.add(material, "wireframe");
+
+gui.addColor(parameters, "color").onChange(() => {
+  material.color.set(parameters.color);
+});
+
+gui.add(parameters, "spin");
 /**
  * Sizes
  */
