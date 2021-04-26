b5aa3c08de78e1bfca1910116cdd2e980d3e120a
Niek <n.veenstra02@gmail.com>
2021-04-26 21:33:02 +0200

    add remove particles function

--- 16-galaxy-generator/src/script.js
+++ 16-galaxy-generator/src/script.js
@@ -7,7 +7,7 @@ import * as dat from "dat.gui";
  * Base
  */
 // Debug
-const gui = new dat.GUI();
+const gui = new dat.GUI({ width: 360 });
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -22,11 +22,21 @@ const parameters = {};
 parameters.count = 1000;
 parameters.size = 0.02;
 
+let geometry = null;
+let material = null;
+let points = null;
+
 const generateGalaxy = () => {
+  // destroy
+  if (points !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
   //
   //
   //
-  const geometry = new THREE.BufferGeometry();
+  geometry = new THREE.BufferGeometry();
 
   const positions = new Float32Array(parameters.count * 3);
 
@@ -42,7 +52,7 @@ const generateGalaxy = () => {
   //
   // materials
   //
-  const material = new THREE.PointsMaterial({
+  material = new THREE.PointsMaterial({
     size: parameters.size,
     sizeAttenuation: true,
     depthWrite: false,
@@ -52,12 +62,14 @@ const generateGalaxy = () => {
   //
   // points
   //
-  const points = new THREE.Points(geometry, material);
+  points = new THREE.Points(geometry, material);
   scene.add(points);
 };
 
 generateGalaxy();
 
+gui.add(parameters, "count").min(100).max(100000).step(100).onFinishChange(generateGalaxy);
+gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
